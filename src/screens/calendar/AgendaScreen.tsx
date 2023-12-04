e5423bf03e5c987c3cdc23d7c304f5f10258ea6c
Gumiero <rigumiero@gmail.com>
2023-12-03 22:49:09 -0300

    update main

--- src/screens/calendar/AgendaScreen.tsx
+++ src/screens/calendar/AgendaScreen.tsx
@@ -1,39 +1,64 @@
 import React, { useEffect, useState } from "react";
-import { Text, StyleSheet, SafeAreaView, View, FlatList } from "react-native";
+import { Text, StyleSheet, SafeAreaView, View } from "react-native";
 import * as SQLite from "expo-sqlite";
-import { Church } from "./interface";
+import { ChurchCalendar } from "./interface";
+import { api } from "../../utils/axios";
+import { Agenda } from "react-native-calendars";
 
 const db = SQLite.openDatabase("church.db");
 
 export const AgendaScreen = () => {
-  const [churches, setChurches] = useState<Church[]>([]);
+  const [churches, setChurches] = useState<ChurchCalendar[]>([]);
+  const [res, setRes] = useState()
 
+  const currentMonth = new Date().getMonth() + 1
+  const currentYear = new Date().getFullYear()
+  const startDateString = `${currentYear}-${currentMonth}-01T00:00:00Z`
+  const startDateStringCal = `${currentYear}-${currentMonth}-02T00:00:00Z`
+  const dateToMessWith = new Date(startDateString)
+  dateToMessWith.setMonth(dateToMessWith.getMonth() + 2, 1)
+  dateToMessWith.setDate(dateToMessWith.getDate() - 2)
+  const endDate = dateToMessWith.toISOString()
+  const endDateFormatted = endDate.slice(0, endDate.indexOf('T'))
+  const endDateString = `${endDateFormatted}T00:00:00Z`
+
+  const url = `/index.php?option=com_dpcalendar&view=events&format=raw&limit=0&Itemid=210&ids=100&date-start=${startDateString}&date-end=${endDateString}`
+  
   useEffect(() => {
-    db.transaction((tx) => {
-      tx.executeSql(
-        "SELECT name FROM church;",
-        [],
-        (_, { rows: { _array } }) => setChurches(_array),
-        (_, err) => {
-          console.error(err);
-          return false;
-        }
-      );
-    });
-  }, []);
-
-  const renderItem = ({ item }: { item: Church }) => (
-    <View style={styles.listItem}>
-      <Text style={styles.name}>{item.name}</Text>
-    </View>
-  );
+    (async () => {
+      const {data} = await api.post(url)
+      const mappedData = data?.data?.map((item: any) => ({
+        title: item?.title,
+        color: item?.backgroundColor,
+        allDay: item?.allDay,
+        startHour: item?.allDay ? undefined : item?.start.slice(item?.start.indexOf('T') + 1, item?.end?.lastIndexOf(':')),
+        endHour: item?.allDay ? undefined : item?.end.slice(item?.end.indexOf('T') + 1, item?.end?.lastIndexOf(':'))
+      }))
+      setRes(mappedData)
+    })()
+  }, [])
 
   return (
     <SafeAreaView style={styles.container}>
-      <FlatList
-        data={churches}
-        renderItem={renderItem}
-        keyExtractor={(item) => item.name}
+      <Agenda
+        markingType="multi-dot"
+        items={{
+          '2023-12-01': [{name: 'Cycling'}, {name: 'Walking'}, {name: 'Running'}],
+          '2023-12-02': [{name: 'Writing'}]
+        }}
+        renderItem={(item, isFirst) => (
+          <View style={styles.item}>
+            <Text style={styles.itemText}>{item.name}</Text>
+          </View>
+        )}
+        renderEmptyData={() => (
+          <View style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
+            <Text style={styles.itemText}>Nenhum evento hoje.</Text>
+          </View>
+        )}
+        minDate={startDateStringCal}
+        maxDate={endDateString}
+        onDayChange={(day) => console.log(day)}
       />
     </SafeAreaView>
   );
@@ -42,6 +67,18 @@ export const AgendaScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  item: {
+    backgroundColor: 'white',
+    flex: 1,
+    borderRadius: 5,
+    padding: 10,
+    marginRight: 10,
+    marginTop: 17,
+  },
+  itemText: {
+    color: '#888',
+    fontSize: 16,
   },
   listItem: {
     padding: 10,
