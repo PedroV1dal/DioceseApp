4d8e05615515868ecc512d3fda6048079ac8e941
= <=>
2023-12-04 12:11:22 -0300

    fix scrool

--- src/screens/churchs/churchsDetails/ChurchDetailsScreen.tsx
+++ src/screens/churchs/churchsDetails/ChurchDetailsScreen.tsx
@@ -2,19 +2,16 @@ import React, { useLayoutEffect } from "react";
 import { useRoute } from "@react-navigation/native";
 import { IChurchs } from "../interface";
 import { useNavigation } from "@react-navigation/native";
-import { Text, Image, StyleSheet, ScrollView } from "react-native";
+import { Text, Image, StyleSheet } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
 
 export const ChurchDetailsScreen = () => {
   const route = useRoute();
   const navigation = useNavigation();
   const { church } = route.params as { church: IChurchs };
 
-  useLayoutEffect(() => {
-    navigation.setOptions({ title: church.name });
-  }, [navigation, church.name]);
-
   return (
-    <ScrollView contentContainerStyle={styles.container}>
+    <ScrollView contentContainerStyle={styles.container} scrollEnabled={true}>
       <Image source={{ uri: church.image }} style={styles.image} />
       <Text style={styles.name}>{church.name}</Text>
       <Text style={styles.address}>{church.address}</Text>
@@ -28,7 +25,7 @@ export const ChurchDetailsScreen = () => {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flexGrow: 1,
     alignItems: "center",
     justifyContent: "flex-start",
   },
